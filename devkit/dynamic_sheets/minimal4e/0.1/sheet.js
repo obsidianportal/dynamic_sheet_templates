2a3d7269d2c3709c74bf568b19ea49181ff9d4c6
Micah <micah@obsidianportal.com>
2010-05-23 14:25:21 -0400

    Updated some comments.

--- devkit/dynamic_sheets/minimal4e/0.1/sheet.js
+++ devkit/dynamic_sheets/minimal4e/0.1/sheet.js
@@ -1,9 +1,13 @@
-// This is the javascript specific to the minimal4e DST
-// In this example, we're using some javascript to update the ability modifiers when someone changes their
-// level or their ability score.
-//
-// The key is to use the callback functions to catch the right events.
-// Read the top comments in characters.js to get a better idea of how the callbacks work.
+/*
+ * This is the javascript specific to the minimal4e DST
+ * In this example, we're using some javascript to update the ability modifiers when someone changes their
+ * level or their ability score.
+ *
+ * The key is to use the callback functions to catch the right events.
+ * Read the top comments in characters.js to get a better idea of how the callbacks work.
+ *
+ * Copy and paste this directly into the javascript textarea on obsidianportal.com
+ */
 
 
 function minimal4e_dataPreLoad(options) {
